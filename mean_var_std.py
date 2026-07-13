38e6e0726345fb04ebccade215438cb2a5935a36
mozhyara <mozhya@gmail.com>
2026-07-13 10:42:45 +0200

    Implement statistics calculation for a 3x3 matrix
    Signed-off-by: mozhyara <mozhya@gmail.com>

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,17 @@
 import numpy as np
 
 def calculate(list):
-
-
-
-
-    return calculations
+    if len(list) != 9:
+        print("List must contain nine numbers.")
+    else:
+        calculations={}
+        y = np.array(list).reshape(3,3)
+        stats = ["mean", "variance", "standard division", "max", "min", "sum"]
+        funs = [y.mean, y.var, y.std, y.max, y.min, y.sum]
+        axes= [0 ,1, None]
+        for stat, fun in zip(stats, funs):
+            calculations[stat] = [fun(axis=ax).tolist() for ax in axes]
+        return calculations
+        
+calculations = calculate([0, 1, 2, 3, 4, 5, 6, 7, 8])
+print(calculations)   
