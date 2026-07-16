b084473ae61e9a55eb32d34c4c24808742188f8c
mozhyara <mozhya@gmail.com>
2026-07-16 11:21:53 +0200

    Clean up mean_var_std.py by removing example usage
    Removed example calculations and print statements.
    
    Signed-off-by: mozhyara <mozhya@gmail.com>

--- mean_var_std.py
+++ mean_var_std.py
@@ -12,6 +12,3 @@ def calculate(list):
         for stat, fun in zip(stats, funs):
             calculations[stat] = [fun(axis=ax).tolist() for ax in axes]
         return calculations
-        
-calculations = calculate([0, 1, 2, 3, 4, 5, 6, 7, 8])
-print(calculations)   
